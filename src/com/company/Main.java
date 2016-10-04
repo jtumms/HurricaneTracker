34e2e09ab8aee00b239d3852cfb7ee2e26b39385
jtumms <john.tumminelli@tummsmedia.com>
2016-10-04 11:20:56 -0400

    Initial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,67 @@
 package com.company;
 
+import spark.ModelAndView;
+import spark.Session;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        HashMap<String, User> users = new HashMap<>();
+
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("loginName");
+                    User user = users.get(name);
+
+                    HashMap m = new HashMap();
+                    if (user != null){
+                        m.put("name", user.name);
+                    }
+                    return new ModelAndView(m, "home.html");
+
+                },
+                new MustacheTemplateEngine()
+        );
+
+        Spark.post(
+                "/login",
+                (request, response) -> {
+                    String name = request.queryParams("loginName");
+                    String password = request.queryParams("password");
+
+                    User user = user.get(name);
+                    if (user == null){
+                        user = new User(name, password);
+                        users.put(name, user);
+                    }
+                    else if (!password.equals(user.password)) {
+                        response.redirect("/");
+                        return null;
+                    }
+                    Session session = request.session();
+                    session.attribute("loginName", name);
+                    response.redirect("/");
+                    return null;
+
+                }
+        );
+        Spark.post(
+                "/logout",
+                (request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return null;
+
+
+                }
+        );
+
     }
 }
---
+++ src/com/company/User.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by john.tumminelli on 10/4/16.
+ */
+public class User {
+    String name;
+    String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
