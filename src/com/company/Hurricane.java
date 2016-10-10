aae3a86028a1d1454749cc5b828272e979cccbfb
jtumms <john.tumminelli@tummsmedia.com>
2016-10-10 19:32:20 -0400

    Initial

--- src/com/company/Hurricane.java
+++ src/com/company/Hurricane.java
@@ -13,7 +13,7 @@ public class Hurricane {
     Category category;
     String image;
 
-    public Hurricane(String name, String location, Category category, String image) {
+    public Hurricane(String name, String location, Category category, String image, User user) {
         this.name = name;
         this.location = location;
         this.category = category;
