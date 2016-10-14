b2ae7e18e401b8be935296859bae745cb2a29a56
jtumms <john.tumminelli@tummsmedia.com>
2016-10-13 20:04:08 -0400

    Added tests for static methods

--- src/com/company/Hurricane.java
+++ src/com/company/Hurricane.java
@@ -4,19 +4,25 @@ package com.company;
  * Created by john.tumminelli on 10/4/16.
  */
 public class Hurricane {
-
-    enum Category {
-        ONE, TWO, THREE, FOUR, FIVE
-    }
+    int id;
     String name;
     String location;
-    Category category;
+    int category;
     String image;
+    String author;
 
-    public Hurricane(String name, String location, Category category, String image, User user) {
+
+
+    public Hurricane(String name, String location, int category, String image, String author , int id) {
+        this.id = id;
         this.name = name;
         this.location = location;
         this.category = category;
         this.image = image;
+        this.author = author;
+
+    }
+
+    public Hurricane() {
     }
 }
