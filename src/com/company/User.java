087cc0ac47cfc79ffff79c2cb4ed0ea65d7acb07
jtumms <john.tumminelli@tummsmedia.com>
2016-10-14 08:53:32 -0400

    Added tests for static methods

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -4,17 +4,20 @@ package com.company;
  * Created by john.tumminelli on 10/4/16.
  */
 public class User {
+    int id;
     String name;
     String password;
-    User user;
 
-    public User(User user) {
-        this.user = user;
-    }
 
     public User(String name, String password) {
         this.name = name;
         this.password = password;
 
+    }
+
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
     }
 }
